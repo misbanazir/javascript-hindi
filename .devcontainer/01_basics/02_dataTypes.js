f786b42d48915dbd411a0104d523c61bab3ed43a
Misba Nazir <100721498+misbanazir@users.noreply.github.com>
2024-06-28 12:14:21 +0000

    dataTypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict;" // treat all js code as newer version
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3
+    +
+3) // code readibility should be high
+
+console.log("misba");
+let name = "misba"
+let age = 20
+let loggedIn = false
+let state;
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undifined => 
+// symbol => used for uniqueness
+
+// object
+console.log(typeof undefined); // undefined
+console.log(typef null); // object 
