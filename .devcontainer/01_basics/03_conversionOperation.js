b070438506790eddbfe2926c1b83fb1b5a4cd31e
Misba Nazir <100721498+misbanazir@users.noreply.github.com>
2024-06-30 10:58:03 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33aabb"
+console.log(typeof (score));
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/* documentation
+"33" => 33
+"33abc" => NaN
+True => 1; False => 0
+*/
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+//"misba" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
